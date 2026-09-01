10e1af63f98889d67cebcf4fd93beb0f1b12fbbc
solaire-lithium-pro <honsougildas@gmail.com>
2026-09-01 07:56:17 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "solar-sizer-v8";
+const CACHE = "solar-sizer-v9";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
